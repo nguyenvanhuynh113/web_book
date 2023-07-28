e3475d98182ae568f1ef5d0008e4b3c040c72db2
NguyenVanHuynh <vanhuynh272001@email.com>
2023-07-28 09:04:27 +0700

    huynh day code len

---
+++ app/Models/Like.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id', 'id_book', 'id_user'
+    ];
+    protected $primaryKey = 'id';
+    protected $table = 'likes';
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
